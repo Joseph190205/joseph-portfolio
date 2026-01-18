8552619a8456b6e74dc08dfa62b55c13dcea1245
Joseph190205 <josephanthoni2005@gmail.com>
2026-01-18 17:46:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
